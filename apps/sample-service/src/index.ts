ae1a0813b7e3574bd8f27d3d382de100c8e83dfc
Rifqi Oktario <oktariorifqi@gmail.com>
2024-02-23 21:46:25 +0700

    CSK-57 - feat(workflow): testing version tagging

--- apps/sample-service/src/index.ts
+++ apps/sample-service/src/index.ts
@@ -13,6 +13,4 @@ app.get('/', (req: Request, res: Response): void => {
 
 app.listen(port, () => {
     Logger.info(`Server is running at http://localhost:${port}`);
-    console.log('Testing Gh Version Tagging');
-    console.log('Testing Gh Version Tagging');
 });
