578c6bb9c328e51699fdde085e20c40931b47579
Rifqi Oktario <oktariorifqi@gmail.com>
2024-02-28 20:12:06 +0700

    CSK-60 - feat(mock-service): add new mock service

---
+++ apps/mock-service/src/routes/DashboardRoutes.ts
@@ -0,0 +1,11 @@
+import express, { Request, Response } from 'express';
+import DashboardResponse from '../resource/dashboard/Dashboard.json';
+import { Logger } from '@siakad/express.utils';
+const router = express.Router();
+
+router.get('/', (req: Request, res: Response): Response => {
+    Logger.info('[Dashboard] Get Dashboard Data');
+    return res.json(DashboardResponse);
+});
+
+export default router;
