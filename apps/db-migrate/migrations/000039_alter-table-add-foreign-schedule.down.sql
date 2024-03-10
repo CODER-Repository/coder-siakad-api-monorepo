e18635debb867a7824a30f5c63d6ea79ac4a9540
Ricky Firmansyah <ricky.firmansyah@dansmultipro.com>
2024-03-10 20:56:49 +0700

    CSK-70 : add foregin up and down

--- apps/db-migrate/migrations/000039_alter-table-add-foreign-schedule.down.sql
+++ apps/db-migrate/migrations/000039_alter-table-add-foreign-schedule.down.sql
@@ -1,11 +1,10 @@
 DROP TABLE IF EXISTS schedule,
-             DROP INDEX IF EXISTS schedule_lecturer_idx,
-             DROP SEQUENCE IF EXISTS schedule_id_sequence;
-
-DROP TYPE IF EXISTS "day";
-
-ALTER TABLE schedule
+    DROP TYPE IF EXISTS "day",
+    DROP INDEX IF EXISTS schedule_lecturer_idx,
+    DROP SEQUENCE IF EXISTS schedule_id_sequence,
     DROP CONSTRAINT IF EXISTS fk_schedule_lecturer_id,
     DROP CONSTRAINT IF EXISTS fk_schedule_course_id,
-    DROP CONSTRAINT IF EXISTS fk_ukt_study_program,
+    DROP CONSTRAINT IF EXISTS fk_schedule_semester_id,
     DROP CONSTRAINT IF EXISTS fk_schedule_class_id;
+
+
--- apps/db-migrate/migrations/000039_alter-table-add-foreign-schedule.up.sql
+++ apps/db-migrate/migrations/000039_alter-table-add-foreign-schedule.up.sql
@@ -1,5 +1,5 @@
 ALTER TABLE schedule
-    ADD CONSTRAINT fk_schedule_lecturer_id FOREIGN KEY (nip) REFERENCES lecturer (nip),
+    ADD CONSTRAINT fk_schedule_lecturer_id FOREIGN KEY (lecturer_id) REFERENCES lecturer (nip),
     ADD CONSTRAINT fk_schedule_course_id FOREIGN KEY (course_id) REFERENCES course (course_id),
     ADD CONSTRAINT fk_schedule_class_id FOREIGN KEY (class_id) REFERENCES class (class_id),
-    ADD CONSTRAINT fk_schedule_semester_id FOREIGN KEY (semester_id) REFERENCES smester (semester_id);
+    ADD CONSTRAINT fk_schedule_semester_id FOREIGN KEY (semester_id) REFERENCES semester (semester_id);
