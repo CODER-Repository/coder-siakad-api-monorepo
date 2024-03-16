cd8dfa54bfd5e300d8131178e915fde4af3d4ee3
Ricky Firmansyah <ricky.firmansyah@dansmultipro.com>
2024-03-17 00:55:45 +0700

    CSK-70: enhance realtion migration

--- apps/db-migrate/migrations/000046_alter_table_changes_and_add_column.up.sql
+++ apps/db-migrate/migrations/000046_alter_table_changes_and_add_column.up.sql
@@ -6,3 +6,18 @@ ALTER COLUMN schedule_id TYPE VARCHAR(20);
 
 ALTER TABLE course
 ADD COLUMN classroom_id VARCHAR(15) NOT NULL;
+
+ALTER TABLE schedule
+ADD CONSTRAINT fk_schedule_student
+FOREIGN KEY (nim)
+REFERENCES student(nim);
+
+ALTER TABLE course
+ADD CONSTRAINT fk_course_classroom
+FOREIGN KEY (classroom_id)
+REFERENCES classroom(classroom_id);
+
+ALTER TABLE class
+ADD CONSTRAINT fk_class_lecturer
+FOREIGN KEY (lecturer_id)
+REFERENCES lecturer(nip);
