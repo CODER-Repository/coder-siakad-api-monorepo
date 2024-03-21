fc528faca1793e6112843481d12982a7df96dea2
Rifqi Oktario <oktariorifqi@gmail.com>
2024-03-21 21:36:02 +0700

    CSK-70 - fix: typo

--- apps/data-service/src/routes/index.ts
+++ apps/data-service/src/routes/index.ts
@@ -1,14 +1,14 @@
 import classRouter from './class-route';
 import lecturerRoute from './lecturer-route';
-import stundentRoute from './student-route';
+import studentRoute from './student-route';
 import classroomRoute from './classroom-route';
 
-const basePath = '/api/v1/data'
+const basePath = '/api/v1/data';
 
 export const routes = [
-  { path: `${basePath}/class`, router: classRouter },
-  { path: `${basePath}/lecturer`, router: lecturerRoute },
-  { path: `${basePath}/student`, router: stundentRoute },
-  { path: `${basePath}/classroom`, router: classroomRoute },
+    { path: `${basePath}/class`, router: classRouter },
+    { path: `${basePath}/lecturer`, router: lecturerRoute },
+    { path: `${basePath}/student`, router: studentRoute },
+    { path: `${basePath}/classroom`, router: classroomRoute }
 ];
 
