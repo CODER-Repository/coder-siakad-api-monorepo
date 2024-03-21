80ecc2cd4f239031f8cbbc716bf72f02e64eeb65
Ricky Firmansyah <ricky.firmansyah@dansmultipro.com>
2024-03-21 14:24:01 +0700

    CSK-75 split: list class and lecturer to data service

--- apps/schedule-service/src/routes/class-route.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import { ClassController } from '../controllers/class-controller';
-
-const classRoute = Router();
-
-classRoute.route('/').get(ClassController.getClass);
-
-export default classRoute;
