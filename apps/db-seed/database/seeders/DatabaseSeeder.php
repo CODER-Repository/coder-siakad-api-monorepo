3b4808eeb231aaa8a0f10b2cbd5830e5853fc65a
007xBramasta <bramasta026x@gmail>
2024-03-23 16:04:01 +0700

    Add KRSSeeder

--- apps/db-seed/database/seeders/DatabaseSeeder.php
+++ apps/db-seed/database/seeders/DatabaseSeeder.php
@@ -18,6 +18,7 @@ class DatabaseSeeder extends Seeder
             ClassRoomSeeder::class,
             AnnouncementSeeder::class,
             StudyProgramSeeder::class,
+            KRSSeeder::class
         ]);
     }
 }
