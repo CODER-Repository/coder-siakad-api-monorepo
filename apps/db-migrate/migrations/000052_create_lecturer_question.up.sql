afe4cae933dfa0ff778e2493f73a8d6e07285a95
Justinus Andreas <firemage0852@gmail.com>
2024-04-06 02:40:19 +0700

    CSK-77: migrate table lecutrer questionnaire

---
+++ apps/db-migrate/migrations/000052_create_lecturer_question.up.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS lecturer_question (
+    id VARCHAR(15) PRIMARY KEY,
+    lecturer_questionnaire_id VARCHAR(15) NOT NULL,
+    question VARCHAR(255) NOT NULL,
+)
+
+ALTER TABLE lecturer_question
+ADD CONSTRAINT fk_lecturer_question_lecturer_questionnaire_id FOREIGN KEY (lecturer_questionnaire_id) REFERENCES "lecturer_questionnaire" (id) ON DELETE CASCADE ON UPDATE CASCADE;
