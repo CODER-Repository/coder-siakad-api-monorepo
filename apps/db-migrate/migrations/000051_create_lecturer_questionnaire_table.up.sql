a7e52b3d18653644225c4564358b5e5962081a5a
Ricky Firmansyah <ricky_firmansyah@teknokrat.ac.id>
2024-04-07 04:04:18 +0700

    CSK-84 fixing: migrate lecturer questioner

--- apps/db-migrate/migrations/000051_create_lecturer_questionnaire_table.up.sql
+++ apps/db-migrate/migrations/000051_create_lecturer_questionnaire_table.up.sql
@@ -1,9 +1,11 @@
 CREATE TABLE IF NOT EXISTS lecturer_questionnaire (
     id VARCHAR(15) PRIMARY KEY,
-    lecturer_id uuid NOT NULL,
-    semester_id VARCHAR(15) NOT NULL,
-)
+    lecturer_id VARCHAR(10) NOT NULL,
+    semester_id VARCHAR(15) NOT NULL
+);
 
 ALTER TABLE lecturer_questionnaire
 ADD CONSTRAINT fk_lecturer_questionnaire_lecturer_id FOREIGN KEY (lecturer_id) REFERENCES "lecturer" (nip) ON DELETE CASCADE ON UPDATE CASCADE;
+
+ALTER TABLE lecturer_questionnaire
 ADD CONSTRAINT fk_lecturer_questionnaire_semester_id FOREIGN KEY (semester_id) REFERENCES "semester" (semester_id) ON DELETE CASCADE ON UPDATE CASCADE;
