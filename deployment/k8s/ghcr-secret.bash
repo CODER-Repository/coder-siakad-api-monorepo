bb224b492cdf4aded1babb2251ecd6e73fb7974c
Rifqi Oktario <oktariorifqi@gmail.com>
2024-06-20 15:49:31 +0700

    CSK-00 - chore: edit deployment and env config

--- deployment/k8s/ghcr-secret.bash
+++ deployment/k8s/ghcr-secret.bash
@@ -1,4 +1,4 @@
 kubectl create secret docker-registry ghcr-secret \
   --docker-server=ghcr.io \
-  --docker-username=coder-automation \
-  --docker-password=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
+  --docker-username={username} \
+  --docker-password={password}
